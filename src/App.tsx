ef857e6838f0571ca82412f0ec59b3747296a42c
Ilya Khrapko <iamdemos461@gmail.com>
2021-07-08 20:53:20 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./Header";
+import Technologies from "./Technologies";
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    )
+}
+
+export default App;
