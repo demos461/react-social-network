428ed33d93b3cbfdbc31ab68b3e1c06591b4eef7
Ilya Khrapko <iamdemos461@gmail.com>
2021-12-10 22:25:19 +0300

    create error 404 page, add switch to route, add redirect, add styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {PureComponent} from 'react';
 import './styles/App.css';
 import Navbar from './components/Navbar/Navbar';
 import MessagesContainer from './components/Messages/MessagesContainer';
-import {Route} from 'react-router-dom';
+import {Redirect, Route, Switch} from 'react-router-dom';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
@@ -11,6 +11,7 @@ import {connect} from 'react-redux';
 import {initializeApp} from "./redux/reducers/app-reducer";
 import {AppRootStateType} from "./redux/store";
 import Preloader from "./components/Preloader/Preloader";
+import Error404 from './components/Error404/Error404';
 
 
 type AppPropsTypeType = {
@@ -34,22 +35,26 @@ class App extends PureComponent<AppPropsTypeType> {
                 <HeaderContainer/>
                 <Navbar/>
                 <div className="app-content-wrapper">
-                    <Route
-                        path={'/profile/:userId?'}
-                        render={() => <ProfileContainer/>}
-                    />
-                    <Route
-                        path={'/messages'}
-                        render={() => <MessagesContainer/>}
-                    />
-                    <Route
-                        path={'/users'}
-                        render={() => <UsersContainer/>}
-                    />
-                    <Route
-                        path={'/login'}
-                        render={() => <Login/>}
-                    />
+                    <Switch>
+                        <Route path={'/'} exact render={() => <Redirect to={'/profile'}/>}/>
+                        <Route
+                            path={'/profile/:userId?'}
+                            render={() => <ProfileContainer/>}
+                        />
+                        <Route
+                            path={'/messages'}
+                            render={() => <MessagesContainer/>}
+                        />
+                        <Route
+                            path={'/users'}
+                            render={() => <UsersContainer/>}
+                        />
+                        <Route
+                            path={'/login'}
+                            render={() => <Login/>}
+                        />
+                        <Route render={() => <Error404/>}/>
+                    </Switch>
                 </div>
             </div>
         );
